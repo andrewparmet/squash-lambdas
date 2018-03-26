9959af5ada7d26fcef470177b45925b2d2ad0619
Andrew Parmet <andrew@parmet.com>
2018-03-26 14:25:14 -0400

    add another test file

--- src/test/java/com/parmet/squashlambdas/MatchRetrieverTest.java
+++ src/test/java/com/parmet/squashlambdas/MatchRetrieverTest.java
@@ -9,7 +9,6 @@ import java.time.Instant;
 import org.junit.Test;
 
 public class MatchRetrieverTest {
-
   @Test
   public void fromEmailDataTest() throws Exception {
     EmailData data = EmailRetrieverTest.emailData();
@@ -27,7 +26,7 @@ public class MatchRetrieverTest {
 
   @Test
   public void testHardballAlone() throws Exception {
-    assertThat(newMatchRetriever("fpdc9cule6ne0okl6jrtdcpv2fpaov031jom6n81").getMatch())
+    assertThat(getMatch(TestUtils.getResourceAsString("fpdc9cule6ne0okl6jrtdcpv2fpaov031jom6n81")))
         .isEqualTo(
             new Match(
                 Sport.HARDBALL,
@@ -37,16 +36,18 @@ public class MatchRetrieverTest {
                 Instant.parse("2018-03-29T01:45:00Z")));
   }
 
-  public static MatchRetriever newMatchRetriever(String fileName) {
+  public static Match getMatch(String body) {
     return new MatchRetriever(
-        new EmailReturningS3(TestUtils.getResourceAsString(fileName)),
-        new S3EmailNotification(
-            "2.0",
-            "aws:s3",
-            "us-east-1",
-            Instant.parse("2018-03-26T00:27:50.117Z"),
-            "ObjectCreated:Put",
-            new S3CreateObjectInfo(
-                new BucketInfo("parmet-squash-emails"), new S3ObjectInfo("emails/" + fileName))));
+            new EmailReturningS3(body),
+            new S3EmailNotification(
+                "2.0",
+                "aws:s3",
+                "us-east-1",
+                Instant.parse("2018-03-26T00:27:50.117Z"),
+                "ObjectCreated:Put",
+                new S3CreateObjectInfo(
+                    new BucketInfo("parmet-squash-emails"),
+                    new S3ObjectInfo("emails/some-file-name"))))
+        .getMatch();
   }
 }
