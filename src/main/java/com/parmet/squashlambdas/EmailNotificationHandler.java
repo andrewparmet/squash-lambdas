5252d482e607114ae04d9a260f1d0ef01df61091
Andrew Parmet <andrew@parmet.com>
2018-06-24 01:15:59 -0400

    get dependencies fixed

--- src/main/java/com/parmet/squashlambdas/EmailNotificationHandler.java
+++ src/main/java/com/parmet/squashlambdas/EmailNotificationHandler.java
@@ -10,24 +10,35 @@ import com.parmet.squashlambdas.email.EmailData;
 import com.parmet.squashlambdas.email.EmailRetriever;
 import com.parmet.squashlambdas.s3.S3CreateObjectInfo;
 import com.parmet.squashlambdas.s3.S3EmailNotification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class EmailNotificationHandler implements RequestHandler<Object, Object> {
+  private static final Logger log = LogManager.getLogger();
+
   private static final AmazonS3 S3 =
       AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
 
   @Override
   public Object handleRequest(Object input, Context context) {
-    S3CreateObjectInfo info = S3EmailNotification.fromInputObject(input).getS3ObjectInfo();
-    EmailData email =
-        new EmailRetriever(S3, info.getBucketName(), info.getObjectKey()).retrieveEmail();
-    ChangeSummary changeSummary = ChangeSummary.fromEmail(email);
+    try {
+      S3CreateObjectInfo info = S3EmailNotification.fromInputObject(input).getS3ObjectInfo();
+      EmailData email =
+          new EmailRetriever(S3, info.getBucketName(), info.getObjectKey()).retrieveEmail();
+      ChangeSummary changeSummary = ChangeSummary.fromEmail(email);
 
-    // TODO: Google API calls
-    // 1. If created, create
-    // 2. If deleted, delete
-    // 3. If player added, delete and create new
+      // TODO: Google API calls
+      // 1. If created, create
+      // 2. If deleted, delete
+      // 3. If player added, delete and create new
 
-    // Catch exception and email myself failure
+      // Catch exception and email myself failure
+
+      log.info(changeSummary);
+    } catch (Throwable t) {
+      String msg = String.format("Caught error while processing input %s", input);
+      log.error(msg, t);
+    }
 
     return "Hello world; received: " + input;
   }
--- src/main/java/com/parmet/squashlambdas/match/MatchRetriever.java
+++ src/main/java/com/parmet/squashlambdas/match/MatchRetriever.java
@@ -1,13 +1,8 @@
 package com.parmet.squashlambdas.match;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Predicates.not;
-import static com.google.common.collect.ImmutableSet.toImmutableSet;
-
-import biweekly.property.Attendee;
 import com.parmet.squashlambdas.email.EmailData;
 import com.parmet.squashlambdas.match.TimeParser.StartAndEnd;
-import java.util.List;
 import java.util.Set;
 
 class MatchRetriever {
@@ -18,7 +13,6 @@ class MatchRetriever {
   }
 
   public Match getMatch() {
-    System.out.println(email.getBody());
     StartAndEnd times = TimeParser.parse(email.getBody());
     return new Match(parseCourt(), parseOtherPlayers(), times.getStart(), times.getEnd());
   }
@@ -29,13 +23,5 @@ class MatchRetriever {
 
   private Set<String> parseOtherPlayers() {
     return OtherPlayersParser.parse(email.getBody());
-  }
-
-  static Set<String> notMe(List<Attendee> attendees) {
-    return attendees
-        .stream()
-        .map(Attendee::getCommonName)
-        .filter(not("Andrew Parmet"::equals))
-        .collect(toImmutableSet());
   }
 }
