c77aa3d15f81286e540cc4ebb4ef27cbd5f3dac3
andrewparmet <7232566+andrewparmet@users.noreply.github.com>
2019-01-27 01:11:15 -0500

    remove ActivityParser from previous Java package-based API and use utilities directly; add direct test coverage

---
+++ src/test/kotlin/com/parmet/squashlambdas/activity/OtherPlayersParserTest.kt
@@ -0,0 +1,21 @@
+package com.parmet.squashlambdas.activity
+
+import com.google.common.truth.Truth.assertThat
+import com.parmet.squashlambdas.integration.emailFromBody
+import org.junit.Test
+
+class OtherPlayersParserTest {
+    @Test
+    fun `an email with no players can be parsed correctly`() {
+        val body = emailFromBody("reservationCreated2").body
+
+        assertThat(OtherPlayersParser.parse(body)).isEmpty()
+    }
+
+    @Test
+    fun `an email with a player can be parsed correctly`() {
+        val body = emailFromBody("reservationCreated3").body
+
+        assertThat(OtherPlayersParser.parse(body)).containsExactly("Philipp Rimmler")
+    }
+}
