b0a3a832f6b5e5b0b21f492f72407772b2533ec7
andrewparmet <7232566+andrewparmet@users.noreply.github.com>
2019-01-27 13:22:23 -0500

    better serialization of action, activity, sport, and court

--- src/main/kotlin/com/parmet/squashlambdas/notify/GsonUtils.kt
+++ src/main/kotlin/com/parmet/squashlambdas/notify/GsonUtils.kt
@@ -1,8 +1,8 @@
 package com.parmet.squashlambdas.notify
 
-import com.fatboyindustrial.gsonjavatime.InstantConverter
-import com.google.gson.GsonBuilder
-import com.google.gson.reflect.TypeToken
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
 import com.google.gson.typeadapters.RuntimeTypeAdapterFactory
 import com.parmet.squashlambdas.activity.Activity
 import com.parmet.squashlambdas.activity.Clinic
@@ -10,51 +10,26 @@ import com.parmet.squashlambdas.activity.Court
 import com.parmet.squashlambdas.activity.Match
 import com.parmet.squashlambdas.activity.Sport
 import com.parmet.squashlambdas.cal.Action
-import java.time.Instant
+import java.lang.reflect.Type
 
-private val BASE_BUILDER =
-    GsonBuilder().registerTypeAdapter(Instant::class.java, InstantConverter())
+internal object SportSerializer : JsonSerializer<Sport> {
+    override fun serialize(src: Sport, typeOfSrc: Type, context: JsonSerializationContext) =
+        JsonPrimitive(src.toString())
+}
 
-private val BASE_GSON =
-    BASE_BUILDER.create()
+internal object CourtSerializer : JsonSerializer<Court> {
+    override fun serialize(src: Court, typeOfSrc: Type, context: JsonSerializationContext) =
+        JsonPrimitive("${src.pretty} (${src.sport})")
+}
 
-internal val SPORT_ADAPTER =
-    RuntimeTypeAdapterFactory.of(Sport::class.java)
-        .registerSubtype(Sport.Squash::class.java)
-        .registerSubtype(Sport.Hardball::class.java)
-        .registerSubtype(Sport.Racquets::class.java)
-        .registerSubtype(Sport.Tennis::class.java)
-        .create(BASE_GSON, TypeToken.get(Sport::class.java))
-
-internal val COURT_ADAPTER =
-    RuntimeTypeAdapterFactory.of(Court::class.java)
-        .registerSubtype(Court.Court1::class.java)
-        .registerSubtype(Court.Court2::class.java)
-        .registerSubtype(Court.Court3::class.java)
-        .registerSubtype(Court.Court5::class.java)
-        .registerSubtype(Court.Court6::class.java)
-        .registerSubtype(Court.Court7::class.java)
-        .registerSubtype(Court.RacquetsCourt::class.java)
-        .registerSubtype(Court.TennisCourt::class.java)
-        .create(
-            BASE_BUILDER.registerTypeAdapter(Sport::class.java, SPORT_ADAPTER).create(),
-            TypeToken.get(Court::class.java))
-
-internal val ACTIVITY_ADAPTER =
+internal val ACTIVITY_ADAPTER_FACTORY =
     RuntimeTypeAdapterFactory.of(Activity::class.java)
         .registerSubtype(Match::class.java)
         .registerSubtype(Clinic::class.java)
-        .create(
-            BASE_BUILDER
-                .registerTypeAdapter(Sport::class.java, SPORT_ADAPTER)
-                .registerTypeAdapter(Court::class.java, COURT_ADAPTER)
-                .create(),
-            TypeToken.get(Activity::class.java))
 
-internal val ACTION_ADAPTER =
-    RuntimeTypeAdapterFactory.of(Action::class.java)
-        .registerSubtype(Action.Create::class.java)
-        .registerSubtype(Action.Delete::class.java)
-        .registerSubtype(Action.Update::class.java)
-        .registerSubtype(Action.None::class.java)
-        .create(BASE_GSON, TypeToken.get(Action::class.java))
+internal object ActionSerializer : ClassNameSerializer<Action>()
+
+internal abstract class ClassNameSerializer<T : Any> : JsonSerializer<T> {
+    override fun serialize(src: T, typeOfSrc: Type, context: JsonSerializationContext) =
+        JsonPrimitive(src::class.simpleName)
+}
