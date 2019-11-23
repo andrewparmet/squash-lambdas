47257f47eb9fc9fe270841427e50b01476e7583b
andrewparmet <7232566+andrewparmet@users.noreply.github.com>
2019-11-23 00:32:37 -0500

    parse csv from octet stream and vnd.ms-excel

--- src/main/kotlin/com/parmet/squashlambdas/email/CsvExtractor.kt
+++ src/main/kotlin/com/parmet/squashlambdas/email/CsvExtractor.kt
@@ -2,5 +2,9 @@ package com.parmet.squashlambdas.email
 
 internal object CsvExtractor : AbstractMimeMessageExtractor<StringBuilder>(::AppendableString) {
     override fun parsers() =
-        listOf(MimeParser.TEXT_CSV)
+        listOf(
+            MimeParser.TEXT_CSV,
+            MimeParser.APPLICATION_OCTET_STREAM_CSV,
+            MimeParser.APPLICATION_VND_MS_EXCEL
+        )
 }
