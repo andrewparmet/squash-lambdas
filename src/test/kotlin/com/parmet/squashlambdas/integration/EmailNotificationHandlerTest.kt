0d650139e37435d8f8e0b5db4617833683ddf68b
andrewparmet <7232566+andrewparmet@users.noreply.github.com>
2025-08-16 18:48:40 -0400

    lint

--- src/test/kotlin/com/parmet/squashlambdas/integration/EmailNotificationHandlerTest.kt
+++ src/test/kotlin/com/parmet/squashlambdas/integration/EmailNotificationHandlerTest.kt
@@ -25,7 +25,6 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.S3Configuration
 import software.amazon.awssdk.services.sns.SnsClient
 import software.amazon.awssdk.services.sns.model.PublishRequest
 import java.time.Instant
